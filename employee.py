82ebe60ebbc639c5799e29f2429928fbfaae18a1
Michael <wanjikumichaelgacha@gmail.com>
2017-10-15 23:04:19 +0300

    class Method

--- employee.py
+++ employee.py
@@ -14,11 +14,29 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
     def apply_raise(self):
-        self.pay = int(self.pay * Employee.raise_amount)
+        self.pay = int(self.pay * self.raise_amount)
+    @classmethod
+    def set_raise_amt (cls, amount):
+        cls.raise_amount = amount
 
-print(Employee.num_of_emps)
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first,last, pay)
+
 
 emp_1 = Employee('Corey', 'Schafer', 50000)
 emp_2 = Employee('Test', 'Employee', 60000)
 
-print(Employee.num_of_emps)
+Employee.set_raise_amt(1.05)
+
+emp_str_1 = 'Michael-Gacha-1500'
+emp_str_2 = 'Mary-Maingi-20000'
+emp_str_4 = 'James-MWiti-1500'
+emp_str_3 = 'Michael-Mangi-3000'
+
+new_emp_1 = Employee.from_string(emp_str_1)
+new_emp_2 = Employee.from_string(emp_str_2)
+
+print(new_emp_1.email)
+print(new_emp_2.pay)
